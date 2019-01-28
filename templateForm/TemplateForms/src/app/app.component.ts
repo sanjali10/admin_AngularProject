63407bbe8610a5a1e1862babb4a956c6fe992dc4
sanjali <sanjali@ssdi.sharp.co.in>
2019-01-28 18:39:08 +0530

    create new project template-form

---
+++ templateForm/TemplateForms/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { User } from "./user";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  topics = ['Angular','React','Vue'];
+  userModel=new User('Rob','Rob@test.com',738728937,'','morning',true)
+}
